15d58255122fbff2c322bdc4609e60d6a77e7ccc
andrew-craig <andrcra@gmail.com>
2021-07-02 23:27:57 -0700

    make source directory an explicit variable in the bash file

--- auto-player.sh
+++ auto-player.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# set the source directory
+SOURCE_DIRECTORY = /home/pi/pi-white-noise
+
 # set the sound file to be played
-SOUND_FILE = /home/pi/pi-white-noise/whitenoise.mp3
+SOUND_FILE = whitenoise.mp3
 
 # other variables
 TEMP_STORAGE=/tmp
-LOG_FILE=~/sleepnoise.log
+LOG_FILE="$SOURCE_DIRECTORY/auto-player.log"
 NOW=$(date "+%Y-%m-%d %H:%M:%S")
 
 # log if mpg123 is not installed
@@ -19,14 +22,14 @@ echo "$NOW - Initialization failed because alsa-utils package is not installed."
 exit 1; }
 
 # log if sound file is missing
-[[ -f "$SOUND_FILE" ]] || {
+[[ -f "$SOURCE_DIRECTORY/$SOUND_FILE" ]] || {
 echo "$NOW - Playback failed because noise file ($SOUND_FILE) not found."  >> $LOG_FILE
 exit 1; }
 
 # copy wav to tmpfs for 0 disk usage
-[[ -f "$TEMP_STORAGE/${SOUND_FILE##*/}" ]] || cp "$SOUND_FILE" "$TEMP_STORAGE"
+[[ -f "$TEMP_STORAGE/$SOUND_FILE" ]] || cp "$SOURCE_DIRECTORY/$SOUND_FILE" "$TEMP_STORAGE"
 
 # play wite noise on infinite loop
-echo "$NOW - Starting noise playback" >> $LOG_FILE
+echo "$NOW - Starting noise machine." >> $LOG_FILE
 amixer cset numid=2 1 &>/dev/null
-mpg123 -a default:CARD=Headphones --loop -1 --gapless --quiet "$TEMP_STORAGE/${SOUND_FILE##*/}" &>/dev/null &
+mpg123 -a default:CARD=Headphones --loop -1 --gapless --quiet "$TEMP_STORAGE/$SOUND_FILE" &>/dev/null &
